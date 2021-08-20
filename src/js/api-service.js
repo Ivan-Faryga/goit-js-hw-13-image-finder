e439b328eee55f7e1efe15e3294d2e5a79ba4fce
Ivan-Faryga <77795237+Ivan-Faryga@users.noreply.github.com>
2021-08-20 22:00:36 +0300

    added api-servise logics

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,2 +1,11 @@
-const BASE_URL = "https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ"
+const BASE_URL = "https://pixabay.com/api/?"; //image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+const apiKey = "22450020-f32da86f0216f1b7b94b605ba";
+
+
+function recieveImgs(request, page) {
+    const fetchImgs = fetch(`${BASE_URL}key=${apiKey}&q=${request}&page=${page}&per_page=12&image_type=photo`).then(response.json());
+    return response.json();
+}
+
+export { recieveImgs };
 
